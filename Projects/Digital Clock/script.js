3acfb1d2fdbfffcf85aedb4eca5bd2cc36cd101e
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-12 14:45:39 +0500

    add styles

--- Projects/Digital Clock/script.js
+++ Projects/Digital Clock/script.js
@@ -1,11 +1,26 @@
 const timeEl = document.getElementById("time");
 const dateEl = document.getElementById("date");
-const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let date;
 
 const updateDate = setInterval(() => {
   date = new Date();
   timeEl.textContent = `${date.getHours()} : ${date.getMinutes()}  : ${date.getSeconds()}`;
-  dateEl.textContent = `${months[date.getMonth()]}, ${days[date.getDay()]} ${date.getDate()}`
+  dateEl.textContent = `${months[date.getMonth()]}, ${
+    days[date.getDay()]
+  } ${date.getDate()}`;
 }, 1000);
