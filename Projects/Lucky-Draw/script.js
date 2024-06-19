9e36533d4f9836f673c3fd29217d3ccf850c6fb5
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-19 19:23:01 +0500

    animation added

--- Projects/Lucky-Draw/script.js
+++ Projects/Lucky-Draw/script.js
@@ -1,8 +1,9 @@
 const inputNames = document.getElementById("input-names");
-const resultEl = document.getElementById("result");
+const outputEl = document.getElementById("result");
 const saveBtn = document.getElementById("save-btn");
 const clearBtn = document.getElementById("clear-btn");
 const drawBtn = document.getElementById("draw-btn");
+const declaration = document.styleSheets[0].cssRules[1];
 let names;
 
 saveBtn.addEventListener("click", () => {
@@ -22,8 +23,17 @@ drawBtn.addEventListener("click", () => {
   const drawIndex = Math.floor(Math.random() * names.length);
   const result = names[drawIndex];
 
-  resultEl.textContent = result;
+  outputEl.style.setProperty("animation-name", "draw");
+  declaration.style.setProperty("--percentage", `${names.length * 100}%`);
+  names.forEach((item) => {
+    outputEl.innerHTML += item + "<br>";
+  });
 
-  names.splice(drawIndex, 1);
-  inputNames.value = names.join("\n");
+  setTimeout(() => {
+    outputEl.style.removeProperty("animation-name");
+    outputEl.textContent = result;
+
+    names.splice(drawIndex, 1);
+    inputNames.value = names.join("\n");
+  }, 10000);
 });
