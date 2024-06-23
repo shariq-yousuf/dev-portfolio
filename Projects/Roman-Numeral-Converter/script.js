6eb3291f2572decbfe20aadc0eb189904ab3ce24
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-23 23:51:00 +0500

    add logic for decimal to roman

--- Projects/Roman-Numeral-Converter/script.js
+++ Projects/Roman-Numeral-Converter/script.js
@@ -1,15 +1,59 @@
 const inputNumber = document.querySelector("#number");
 const convertBtn = document.querySelector("#convert-btn");
 const outputEl = document.querySelector("#output");
+const romanNumeral = {
+  1: "I",
+  4: "IV",
+  5: "V",
+  9: "IX",
+  10: "X",
+  40: "XL",
+  50: "L",
+  90: "XC",
+  100: "C",
+  400: "CD",
+  500: "D",
+  900: "CM",
+  1000: "M",
+};
+
+const checkInputNumber = () => {
+  const inputValue = inputNumber.value;
+
+  if (inputValue === "") {
+    outputEl.textContent = "Please enter a valid number.";
+  } else if (inputValue < 1) {
+    outputEl.textContent = "Please enter a number greater than or equal to 1.";
+  } else if (inputValue >= 4000) {
+    outputEl.textContent = "Please enter a number less than or equal to 3999.";
+  } else {
+    decimalToRoman();
+  }
+};
+
+const decimalToRoman = () => {
+  outputEl.textContent = "";
+  const decimalKeys = Object.keys(romanNumeral).reverse();
+  let inputValue = parseInt(inputNumber.value);
+
+  for (const key of decimalKeys) {
+    if (inputValue >= parseInt(key)) {
+      const quotient = Math.floor(inputValue / key);
+      const remainder = inputValue % key;
+
+      for (let i = 0; i < quotient; i++) {
+        outputEl.textContent += romanNumeral[key];
+      }
+
+      inputValue = remainder;
+    }
+  }
+
+  inputNumber.value = "";
+};
 
 convertBtn.addEventListener("click", () => {
-  if (inputNumber.value === "") {
-    outputEl.textContent = "Please enter a valid number.";
-  } else if (inputNumber.value < 1) {
-    outputEl.textContent = "Please enter a number greater than or equal to 1.";
-  } else if (inputNumber.value === "4000") {
-    outputEl.textContent = "Please enter a number less than or equal to 3999.";
-  }
+  checkInputNumber();
 });
 
 inputNumber.addEventListener("keydown", (e) => {
