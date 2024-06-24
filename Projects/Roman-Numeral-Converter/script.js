3d3d7890717992aaef834fd711710bcacce902f9
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-24 13:12:23 +0500

    refactor js

--- Projects/Roman-Numeral-Converter/script.js
+++ Projects/Roman-Numeral-Converter/script.js
@@ -22,9 +22,9 @@ const checkInputNumber = () => {
 
   if (inputValue === "") {
     outputEl.textContent = "Please enter a valid number.";
-  } else if (inputValue < 1) {
+  } else if (parseInt(inputValue) < 1) {
     outputEl.textContent = "Please enter a number greater than or equal to 1.";
-  } else if (inputValue >= 4000) {
+  } else if (parseInt(inputValue) >= 4000) {
     outputEl.textContent = "Please enter a number less than or equal to 3999.";
   } else {
     decimalToRoman();
@@ -33,28 +33,26 @@ const checkInputNumber = () => {
 
 const decimalToRoman = () => {
   outputEl.textContent = "";
+
   const decimalKeys = Object.keys(romanNumeral).reverse();
   let inputValue = parseInt(inputNumber.value);
 
   for (const key of decimalKeys) {
     if (inputValue >= parseInt(key)) {
       const quotient = Math.floor(inputValue / key);
-      const remainder = inputValue % key;
 
       for (let i = 0; i < quotient; i++) {
         outputEl.textContent += romanNumeral[key];
       }
 
-      inputValue = remainder;
+      inputValue = inputValue % key;
     }
   }
 
   inputNumber.value = "";
 };
 
-convertBtn.addEventListener("click", () => {
-  checkInputNumber();
-});
+convertBtn.addEventListener("click", checkInputNumber);
 
 inputNumber.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
