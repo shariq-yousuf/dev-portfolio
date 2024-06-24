ee40465931ec3a16030defb80a8b0e4cc109056f
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-24 16:15:44 +0500

    update date in coming soon and improvised style

--- Projects/Coming-Soon/coming-soon.js
+++ Projects/Coming-Soon/coming-soon.js
@@ -1,8 +1,24 @@
 const timerDays = document.getElementById("timer-days");
 const timerHours = document.getElementById("timer-hours");
+const dateEl = document.getElementById("date");
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const date = new Date(2025, 0, 1);
 
 const timer = setInterval(() => {
-  const date = new Date(2024, 5, 22);
   const currentDate = new Date();
   const diff = date - currentDate;
 
@@ -23,3 +39,7 @@ const timer = setInterval(() => {
     .toString()
     .padStart(2, 0)} : ${sec.toString().padStart(2, 0)}`;
 }, 1000);
+
+dateEl.textContent = `${date.getDate().toString().padStart(2, 0)}-${
+  months[date.getMonth()]
+}-${date.getFullYear()}`;
