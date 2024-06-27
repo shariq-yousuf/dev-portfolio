24b6d4db0c4717c8ab95734e2935e8dedc65cedd
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-28 00:02:52 +0500

    now user can see detailed quiz result at the end

--- Projects/Quiz-App/script.js
+++ Projects/Quiz-App/script.js
@@ -1,7 +1,9 @@
+const mainContainer = document.querySelector("#main-container");
 const spinnerContainer = document.querySelector("#spinner-container");
 const spinner = document.querySelector("#spinner");
 const quizEl = document.querySelector("#quiz");
 const nextBtn = document.querySelector("#next-btn");
+const quizAgainBtn = document.createElement("button");
 
 // const quizData = [
 //   {
@@ -62,8 +64,10 @@ const nextBtn = document.querySelector("#next-btn");
 // ];
 let quizData;
 let currentQuestionIndex = 0;
-let userAnswer;
+let selectedElements = [];
 const correctAnswers = new Set();
+let score;
+const resultData = [];
 
 const url =
   "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple";
@@ -93,12 +97,13 @@ const getQuizData = async () => {
 };
 
 const startQuiz = (quizData) => {
+  quizAgainBtn.style.display = "none";
   currentQuestionIndex = 0;
   correctAnswers.clear();
   renderQuestion(currentQuestionIndex, quizData);
 
   nextBtn.textContent = "Next";
-  nextBtn.removeEventListener("click", startQuiz);
+  nextBtn.removeEventListener("click", showResult);
   nextBtn.addEventListener("click", nextQuestion);
 };
 
@@ -109,7 +114,7 @@ const renderQuestion = (number, quizData) => {
   const { id, question, incorrect_answers, correct_answer } = quizData[number];
   const answers = [...incorrect_answers, correct_answer].sort();
 
-  quizEl.innerHTML = `
+  const quizHTML = `
         <span class="text-base absolute right-0 top-0">${id
           .toString()
           .padStart(2, 0)}/${quizData.length.toString().padStart(2, 0)}
@@ -119,87 +124,155 @@ const renderQuestion = (number, quizData) => {
         </p>
         <ul class="options w-11/12 mx-auto cursor-pointer">
           <li class="hover:bg-sky-300  rounded-3xl">
-            <label class="cursor-pointer block px-4 py-2" for="opt-1">
+            <label class="cursor-pointer block px-4 py-2" for="Q-${id}-opt-1">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-1" value="${
-    answers[0]
-  }"/>
-            ${answers[0]}</label>
+            class="me-2" type="radio" name="${id}" id="Q-${id}-opt-1" 
+            value="${answers[0]}"/>${answers[0]}</label>
           </li>
           <li class="hover:bg-sky-300 rounded-3xl">
-            <label class="cursor-pointer block px-4 py-2" for="opt-2">
+            <label class="cursor-pointer block px-4 py-2" for="Q-${id}-opt-2">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-2" value="${
-    answers[1]
-  }"/>
-            ${answers[1]}</label>
+            class="me-2" type="radio" name="${id}" id="Q-${id}-opt-2" 
+            value="${answers[1]}"/>${answers[1]}</label>
           </li>
           <li class="hover:bg-sky-300 rounded-3xl">
-            <label class="cursor-pointer block px-4 py-2" for="opt-3">
+            <label class="cursor-pointer block px-4 py-2" for="Q-${id}-opt-3">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-3" value="${
-    answers[2]
-  }"/>
-            ${answers[2]}</label>
+            class="me-2" type="radio" name="${id}" id="Q-${id}-opt-3" 
+            value="${answers[2]}"/>${answers[2]}</label>
           </li>
           <li class="hover:bg-sky-300 rounded-3xl">
-            <label class="cursor-pointer block px-4 py-2" for="opt-4">
+            <label class="cursor-pointer block px-4 py-2" for="Q-${id}-opt-4">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-4" value="${
-    answers[3]
-  }"/>
-            ${answers[3]}</label>
+            class="me-2" type="radio" name="${id}" id="Q-${id}-opt-4" 
+            value="${answers[3]}"/>${answers[3]}</label>
           </li>
         </ul>
       `;
 
+  quizEl.innerHTML = quizHTML;
+  resultData.push(quizHTML);
+
   if (currentQuestionIndex === quizData.length - 1) {
-    setTimeout(() => {
-      nextBtn.textContent = "Submit";
-    }, 500);
+    nextBtn.textContent = "Submit";
   }
 };
 
 function getUserAnswer(inputEl) {
   nextBtn.removeAttribute("disabled");
 
-  userAnswer = inputEl.value;
-  checkUserAnswer();
+  selectedElements.push(inputEl);
+  checkUserAnswer(inputEl.value);
 }
 
-function checkUserAnswer() {
-  if (userAnswer === quizData[currentQuestionIndex].correct_answer) {
-    correctAnswers.add(userAnswer);
+function checkUserAnswer(inputValue) {
+  if (inputValue === quizData[currentQuestionIndex].correct_answer) {
+    correctAnswers.add(inputValue);
   }
 }
 
 const nextQuestion = () => {
-  console.log(quizData);
   if (currentQuestionIndex < quizData.length - 1) {
     currentQuestionIndex++;
     renderQuestion(currentQuestionIndex, quizData);
   } else {
     const percentageScore = (correctAnswers.size / quizData.length) * 100;
 
-    quizEl.innerHTML = `${
+    score = `${
       correctAnswers.size > 1 ? "Correct Answers:" : "Correct Answer:"
     } ${correctAnswers.size} out of ${quizData.length} <br/>
      Score: ${parseInt(percentageScore)}% <br/>
      `;
+    quizEl.innerHTML = score;
+
+    nextBtn.textContent = "Show Result";
+    nextBtn.removeEventListener("click", nextQuestion);
+    nextBtn.addEventListener("click", showResult);
 
     if (correctAnswers.size < quizData.length) {
       quizEl.innerHTML += "Want to try again?";
 
-      nextBtn.textContent = "Take Quiz Again";
-      nextBtn.removeEventListener("click", nextQuestion);
-      nextBtn.addEventListener("click", () => {
+      quizAgainBtn.style.display = "block";
+      quizAgainBtn.textContent = "Take Quiz Again";
+      quizAgainBtn.classList.add(
+        "px-4",
+        "py-2",
+        "md:px-7",
+        "md:py-3",
+        "text-xl",
+        "font-bold",
+        "rounded-xl",
+        "hover:bg-indigo-600",
+        "mt-4",
+        "block",
+        "ms-auto",
+        "bg-indigo-700",
+        "text-white",
+        "tansition-all",
+        "duration-500"
+      );
+      quizAgainBtn.addEventListener("click", () => {
         startQuiz(quizData);
       });
+      mainContainer.appendChild(quizAgainBtn);
     } else {
       quizEl.innerHTML += `Congrats! You got all ${quizData.length} questions right.`;
-      nextBtn.style.display = "none";
     }
   }
+};
+
+const showResult = () => {
+  mainContainer.innerHTML = "";
+  const scoreEl = document.createElement("div");
+  scoreEl.classList.add("text-xl", "font-bold", "m-4", "text-center");
+  scoreEl.innerHTML = score;
+  mainContainer.appendChild(scoreEl);
+
+  let i = 0;
+
+  resultData.forEach((item) => {
+    const div = document.createElement("div");
+    div.classList.add("text-sky-800", "relative", "font-bold", "text-xl");
+    div.innerHTML = item;
+    const showCorrectAnsEl = document.createElement("p");
+    showCorrectAnsEl.classList.add("m-2", "text-right", "text-green-700");
+    showCorrectAnsEl.innerHTML = `Correct Answer: ${quizData[i].correct_answer}`;
+    div.appendChild(showCorrectAnsEl);
+    mainContainer.appendChild(div);
+    const inputNodes = document.querySelectorAll("input");
+    const liNodes = document.querySelectorAll("li");
+    for (const node of inputNodes) {
+      for (const selected of selectedElements) {
+        if (selected.getAttribute("id") === node.getAttribute("id")) {
+          if (correctAnswers.has(node.value)) {
+            node.parentElement.classList.add(
+              "bg-green-300",
+              "rounded-3xl",
+              "border-2",
+              "border-solid",
+              "border-green-800"
+            );
+            showCorrectAnsEl.style.display = "none";
+          } else {
+            node.parentElement.classList.add(
+              "bg-red-300",
+              "rounded-3xl",
+              "border-2",
+              "border-solid",
+              "border-red-900"
+            );
+            showCorrectAnsEl.style.display = "block";
+          }
+        }
+      }
+
+      node.setAttribute("disabled", "disabled");
+    }
+    for (const node of liNodes) {
+      node.classList.remove("hover:bg-sky-300");
+    }
+    i += 1;
+  });
 };
 
 window.addEventListener("load", getQuizData);
