4edcd53a160c37916ff3559d3a189f16a7b6be89
shariq-yousuf <shariqyousuf01@gmail.com>
2024-06-27 17:41:56 +0500

    fetch quiz data from API, and display API data in the quiz and it changes every time the page refresh

--- Projects/Quiz-App/script.js
+++ Projects/Quiz-App/script.js
@@ -1,86 +1,95 @@
 const quizEl = document.querySelector("#quiz");
 const nextBtn = document.querySelector("#next-btn");
-const quizData = [
-  {
-    id: 1,
-    question: "What is the largest planet in our solar system?",
-    answers: {
-      a: "Mars",
-      b: "Jupiter",
-      c: "Saturn",
-      d: "Earth",
-    },
-    correctAnswer: "b",
-  },
-  {
-    id: 2,
-    question: "Which famous scientist developed the theory of relativity?",
-    answers: {
-      a: "Isaac Newton",
-      b: "Stephen Hawking",
-      c: "Galileo Galilei",
-      d: "Albert Einstein",
-    },
-    correctAnswer: "d",
-  },
-  {
-    id: 3,
-    question: "Which country is known as the Land of the Rising Sun?",
-    answers: {
-      a: "China",
-      b: "India",
-      c: "Japan",
-      d: "Thailand",
-    },
-    correctAnswer: "c",
-  },
-  {
-    id: 4,
-    question: "Who painted the famous work 'Mona Lisa'?",
-    answers: {
-      a: "Leonardo da Vinci",
-      b: "Vincent van Gogh",
-      c: "Pablo Picasso",
-      d: "Michelangelo",
-    },
-    correctAnswer: "a",
-  },
-  {
-    id: 5,
-    question: "What is the capital city of Australia?",
-    answers: {
-      a: "Sydney",
-      b: "Canberra",
-      c: "Melbourne",
-      d: "Brisbane",
-    },
-    correctAnswer: "b",
-  },
-];
 
+// const quizData = [
+//   {
+//     id: 1,
+//     question: "What is the largest planet in our solar system?",
+//     answers: {
+//       a: "Mars",
+//       b: "Jupiter",
+//       c: "Saturn",
+//       d: "Earth",
+//     },
+//     correctAnswer: "b",
+//   },
+//   {
+//     id: 2,
+//     question: "Which famous scientist developed the theory of relativity?",
+//     answers: {
+//       a: "Isaac Newton",
+//       b: "Stephen Hawking",
+//       c: "Galileo Galilei",
+//       d: "Albert Einstein",
+//     },
+//     correctAnswer: "d",
+//   },
+//   {
+//     id: 3,
+//     question: "Which country is known as the Land of the Rising Sun?",
+//     answers: {
+//       a: "China",
+//       b: "India",
+//       c: "Japan",
+//       d: "Thailand",
+//     },
+//     correctAnswer: "c",
+//   },
+//   {
+//     id: 4,
+//     question: "Who painted the famous work 'Mona Lisa'?",
+//     answers: {
+//       a: "Leonardo da Vinci",
+//       b: "Vincent van Gogh",
+//       c: "Pablo Picasso",
+//       d: "Michelangelo",
+//     },
+//     correctAnswer: "a",
+//   },
+//   {
+//     id: 5,
+//     question: "What is the capital city of Australia?",
+//     answers: {
+//       a: "Sydney",
+//       b: "Canberra",
+//       c: "Melbourne",
+//       d: "Brisbane",
+//     },
+//     correctAnswer: "b",
+//   },
+// ];
+let quizData;
 let currentQuestionIndex = 0;
 let userAnswer;
 const correctAnswers = new Set();
 
-const startQuiz = () => {
+const url =
+  "https://opentdb.com/api.php?amount=10&category=23&difficulty=easy&type=multiple";
+
+const getQuizData = async () => {
+  const response = await fetch(url);
+  const result = await response.json();
+  quizData = result.results;
+
+  startQuiz(quizData);
+};
+
+const startQuiz = (quizData) => {
   currentQuestionIndex = 0;
   correctAnswers.clear();
-  renderQuestion(currentQuestionIndex);
+  renderQuestion(currentQuestionIndex, quizData);
 
   nextBtn.textContent = "Next";
   nextBtn.removeEventListener("click", startQuiz);
   nextBtn.addEventListener("click", nextQuestion);
 };
 
-const renderQuestion = (number) => {
+const renderQuestion = (number, quizData) => {
   nextBtn.setAttribute("disabled", "disabled");
 
-  const {
-    id,
-    question: que,
-    answers: ans,
-    correctAnswer: correct,
-  } = quizData[number];
+  quizData[number].id = number + 1;
+  const { id, question, incorrect_answers, correct_answer } = quizData[number];
+  const answers = [...incorrect_answers, correct_answer].sort();
 
   quizEl.innerHTML = `
         <span class="text-base absolute right-0 top-0">${id
@@ -88,32 +97,40 @@ const renderQuestion = (number) => {
           .padStart(2, 0)}/${quizData.length.toString().padStart(2, 0)}
         </span>
         <p class="question my-4 md:m-8">
-          ${que}
+          ${question}
         </p>
         <ul class="options w-11/12 mx-auto cursor-pointer">
           <li class="hover:bg-sky-300  rounded-3xl">
             <label class="cursor-pointer block px-4 py-2" for="opt-1">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-1" value="${ans.a}"/>
-            ${ans.a}</label>
+            class="me-2" type="radio" name="${id}" id="opt-1" value="${
+    answers[0]
+  }"/>
+            ${answers[0]}</label>
           </li>
           <li class="hover:bg-sky-300 rounded-3xl">
             <label class="cursor-pointer block px-4 py-2" for="opt-2">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-2" value="${ans.b}"/>
-            ${ans.b}</label>
+            class="me-2" type="radio" name="${id}" id="opt-2" value="${
+    answers[1]
+  }"/>
+            ${answers[1]}</label>
           </li>
           <li class="hover:bg-sky-300 rounded-3xl">
             <label class="cursor-pointer block px-4 py-2" for="opt-3">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-3" value="${ans.c}"/>
-            ${ans.c}</label>
+            class="me-2" type="radio" name="${id}" id="opt-3" value="${
+    answers[2]
+  }"/>
+            ${answers[2]}</label>
           </li>
           <li class="hover:bg-sky-300 rounded-3xl">
             <label class="cursor-pointer block px-4 py-2" for="opt-4">
             <input onclick="getUserAnswer(this)"
-            class="me-2" type="radio" name="${id}" id="opt-4" value="${ans.d}"/>
-            ${ans.d}</label>
+            class="me-2" type="radio" name="${id}" id="opt-4" value="${
+    answers[3]
+  }"/>
+            ${answers[3]}</label>
           </li>
         </ul>
       `;
@@ -133,20 +150,16 @@ function getUserAnswer(inputEl) {
 }
 
 function checkUserAnswer() {
-  if (
-    userAnswer ===
-    quizData[currentQuestionIndex].answers[
-      quizData[currentQuestionIndex].correctAnswer
-    ]
-  ) {
+  if (userAnswer === quizData[currentQuestionIndex].correct_answer) {
     correctAnswers.add(userAnswer);
   }
 }
 
 const nextQuestion = () => {
+  console.log(quizData);
   if (currentQuestionIndex < quizData.length - 1) {
     currentQuestionIndex++;
-    renderQuestion(currentQuestionIndex);
+    renderQuestion(currentQuestionIndex, quizData);
   } else {
     const percentageScore = (correctAnswers.size / quizData.length) * 100;
 
@@ -161,7 +174,9 @@ const nextQuestion = () => {
 
       nextBtn.textContent = "Take Quiz Again";
       nextBtn.removeEventListener("click", nextQuestion);
-      nextBtn.addEventListener("click", startQuiz);
+      nextBtn.addEventListener("click", () => {
+        startQuiz(quizData);
+      });
     } else {
       quizEl.innerHTML += `Congrats! You got all ${quizData.length} questions right.`;
       nextBtn.style.display = "none";
@@ -169,4 +184,4 @@ const nextQuestion = () => {
   }
 };
 
-window.addEventListener("load", startQuiz);
+window.addEventListener("load", getQuizData);
