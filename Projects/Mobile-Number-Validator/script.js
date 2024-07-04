05b009e15a22f6e4bde096fa49e7d311a07204b4
shariq-yousuf <shariqyousuf01@gmail.com>
2024-07-04 23:31:02 +0500

    add styles

--- Projects/Mobile-Number-Validator/script.js
+++ Projects/Mobile-Number-Validator/script.js
@@ -8,7 +8,7 @@ const USANumberFormats = [
   /^1?\s?(\([0-9]{3}\))[\s-]?([0-9]{3})[\s-]?([0-9]{4})$/,
 ];
 
-const PakNumberFormat = [/^[(0092)(\+92)(0)][0-9]{3}[\s-]?[0-9]{7}$/];
+// const PakNumberFormat = [/^[(0092)(\+92)(0)][0-9]{3}[\s-]?[0-9]{7}$/];
 
 const checkUserInput = () => {
   const inputValue = inputEl.value.trim();
@@ -19,16 +19,24 @@ const checkUserInput = () => {
     for (const regex of USANumberFormats) {
       const isValid = regex.test(inputValue);
       if (isValid) {
-        resultEl.innerHTML = `Valid US number: ${inputEl.value}`;
+        resultEl.innerHTML = `Valid US number: <br> ${inputEl.value}`;
         break;
       } else {
-        resultEl.innerHTML = `Invalid US number: ${inputEl.value}`;
+        resultEl.innerHTML = `Invalid US number: <br> ${inputEl.value}`;
       }
     }
   }
+
+  inputEl.value = "";
 };
 
 checkBtn.addEventListener("click", checkUserInput);
 clearBtn.addEventListener("click", () => {
   resultEl.textContent = "";
+});
+inputEl.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    e.preventDefault();
+    checkUserInput();
+  }
 });
