c8645c0ff5ff9796f714787748437fb7e9357579
shariq-yousuf <shariqyousuf01@gmail.com>
2024-07-04 21:07:05 +0500

    add js now it's functional for usa number validation

---
+++ Projects/Mobile-Number-Validator/script.js
@@ -0,0 +1,34 @@
+const inputEl = document.querySelector("#user-input");
+const checkBtn = document.querySelector("#check-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const resultEl = document.querySelector("#results-div");
+
+const USANumberFormats = [
+  /^(1\s)?([0-9]{3})[\s-]?([0-9]{3})[\s-]?([0-9]{4})$/,
+  /^1?\s?(\([0-9]{3}\))[\s-]?([0-9]{3})[\s-]?([0-9]{4})$/,
+];
+
+const PakNumberFormat = [/^[(0092)(\+92)(0)][0-9]{3}[\s-]?[0-9]{7}$/];
+
+const checkUserInput = () => {
+  const inputValue = inputEl.value.trim();
+
+  if (!inputValue) {
+    alert("Please provide a phone number");
+  } else {
+    for (const regex of USANumberFormats) {
+      const isValid = regex.test(inputValue);
+      if (isValid) {
+        resultEl.innerHTML = `Valid US number: ${inputEl.value}`;
+        break;
+      } else {
+        resultEl.innerHTML = `Invalid US number: ${inputEl.value}`;
+      }
+    }
+  }
+};
+
+checkBtn.addEventListener("click", checkUserInput);
+clearBtn.addEventListener("click", () => {
+  resultEl.textContent = "";
+});
