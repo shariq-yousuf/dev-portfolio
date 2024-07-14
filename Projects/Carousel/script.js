0399392793ac6856f1b1a218f18f4aafb29cb19f
shariq-yousuf <shariqyousuf01@gmail.com>
2024-07-14 17:00:32 +0500

    feat: add dots to slides

--- Projects/Carousel/script.js
+++ Projects/Carousel/script.js
@@ -1,10 +1,13 @@
 const rightBtn = document.querySelector(".right-btn");
 const leftBtn = document.querySelector(".left-btn");
 const slideContainer = document.querySelector(".slide-container");
+const dotContainer = document.querySelector(".dot-container");
 
 let position = 0;
 
 const slides = [];
+let dotId = 0;
+
 const imgs = [
   "./pics/pic(1).jpg",
   "./pics/pic(2).jpg",
@@ -27,6 +30,38 @@ imgs.forEach((img) => {
 
 slides.forEach((slide) => slideContainer.appendChild(slide));
 
+slides.forEach(() => {
+  const dotEl = document.createElement("div");
+  dotEl.classList.add("dot");
+  dotEl.id = dotId;
+
+  dotContainer.appendChild(dotEl);
+
+  dotId += 100;
+});
+
+const goToSlide = (e) => {
+  position = e.target.id;
+
+  slides.forEach((slide) => {
+    slide.style.left = `-${position}%`;
+  });
+
+  highlightDot();
+};
+
+const highlightDot = () => {
+  const dots = Array.from(document.getElementsByClassName("dot"));
+  dots.forEach((dot) => {
+    dot.classList.remove("focus");
+    dot.addEventListener("click", goToSlide);
+  });
+
+  const targetDot = dots.find((dot) => parseInt(dot.id) === Math.abs(position));
+
+  targetDot.classList.add("focus");
+};
+
 rightBtn.addEventListener("click", () => {
   if (position > (slides.length - 1) * -100) {
     position -= 100;
@@ -39,6 +74,8 @@ rightBtn.addEventListener("click", () => {
       slide.style.left = `${position}%`;
     });
   }
+
+  highlightDot();
 });
 
 leftBtn.addEventListener("click", () => {
@@ -53,4 +90,10 @@ leftBtn.addEventListener("click", () => {
       slide.style.left = `${position}%`;
     });
   }
+
+  highlightDot();
 });
+
+// document.getElementsByClassName("dot")
+
+highlightDot();
