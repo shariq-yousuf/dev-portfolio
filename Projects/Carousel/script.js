dbce627142ee7d6851a385d8dfa245178a4483b9
shariq-yousuf <shariqyousuf01@gmail.com>
2024-07-14 15:40:46 +0500

    chore: add imgs and made funcionality better

--- Projects/Carousel/script.js
+++ Projects/Carousel/script.js
@@ -1,19 +1,46 @@
 const rightBtn = document.querySelector(".right-btn");
 const leftBtn = document.querySelector(".left-btn");
-const slides = document.getElementsByClassName("slide");
+const slideContainer = document.querySelector(".slide-container");
 
 let position = 0;
 
+const slides = [];
+const imgs = [
+  "./pics/pic(1).jpg",
+  "./pics/pic(2).jpg",
+  "./pics/pic(3).jpg",
+  "./pics/pic(4).jpg",
+  "./pics/pic(5).jpg",
+];
+
+imgs.forEach((img) => {
+  const slideEl = document.createElement("div");
+  slideEl.classList.add("slide");
+
+  const imgEl = document.createElement("img");
+  imgEl.classList.add("slide-img");
+  imgEl.setAttribute("src", `${img}`);
+
+  slideEl.appendChild(imgEl);
+  slides.push(slideEl);
+});
+
+slides.forEach((slide) => slideContainer.appendChild(slide));
+
 rightBtn.addEventListener("click", () => {
-  position -= 100;
-  Array.from(slides).forEach((slide) => {
-    slide.style.left = `${position}%`;
-  });
+  if (position > (slides.length - 1) * -100) {
+    position -= 100;
+    slides.forEach((slide) => {
+      slide.style.left = `${position}%`;
+    });
+  }
 });
 
 leftBtn.addEventListener("click", () => {
-  position += 100;
-  Array.from(slides).forEach((slide) => {
-    slide.style.left = `${position}%`;
-  });
+  if (position < 0) {
+    position += 100;
+    slides.forEach((slide) => {
+      slide.style.left = `${position}%`;
+    });
+  }
 });
