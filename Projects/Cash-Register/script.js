f994f64c0fabc6479304ae1c2e29fba5a6e16801
shariq-yousuf <shariqyousuf01@gmail.com>
2024-07-18 14:49:20 +0500

    feat: add data and algorithm for PKR calculation and add clear btn

--- Projects/Cash-Register/script.js
+++ Projects/Cash-Register/script.js
@@ -1,32 +1,63 @@
 const inputEl = document.querySelector("#cash");
 const changeDueEl = document.querySelector("#change-due");
 const purchaseBtn = document.querySelector("#purchase-btn");
+const clearBtn = document.querySelector("#clear-btn");
+const currencyType = document.querySelector("#currency-type");
 
-let price = 1;
-let cid = [
-  ["PENNY", 1.01],
-  ["NICKEL", 2.05],
-  ["DIME", 3.1],
-  ["QUARTER", 4.25],
-  ["ONE", 90],
-  ["FIVE", 55],
-  ["TEN", 20],
-  ["TWENTY", 60],
-  ["ONE HUNDRED", 100],
-];
-
-const currencyValues = {
-  PENNY: 0.01,
-  NICKEL: 0.05,
-  DIME: 0.1,
-  QUARTER: 0.25,
-  ONE: 1,
-  FIVE: 5,
-  TEN: 10,
-  TWENTY: 20,
-  "ONE HUNDRED": 100,
+let cid = {
+  PKR: [
+    ["ONE", 60],
+    ["TWO", 40],
+    ["FIVE", 70],
+    ["TEN", 330],
+    ["TWENTY", 480],
+    ["FIFTY", 750],
+    ["HUNDRED", 1600],
+    ["FIVE HUNDRED", 2500],
+    ["THOUSAND", 5000],
+    ["FIVE THOUSAND", 10000],
+  ],
+  USD: [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100],
+  ],
 };
 
+const currencyValues = [
+  {
+    ONE: 1,
+    TWO: 2,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    FIFTY: 50,
+    HUNDRED: 100,
+    "FIVE HUNDRED": 500,
+    THOUSAND: 1000,
+    "FIVE THOUSAND": 5000,
+  },
+  {
+    PENNY: 0.01,
+    NICKEL: 0.05,
+    DIME: 0.1,
+    QUARTER: 0.25,
+    ONE: 1,
+    FIVE: 5,
+    TEN: 10,
+    TWENTY: 20,
+    "ONE HUNDRED": 100,
+  },
+];
+
+let price = 52;
+let isPKR = true;
 let changeDue;
 
 const checkPurchase = (val) => {
@@ -41,12 +72,13 @@ const checkPurchase = (val) => {
 
 const checkDrawer = (val) => {
   const cashDue = val - price;
-  const drawerCash = cid.reduce((acc, item) => item[1] + acc, 0);
+  const cidType = isPKR ? cid.PKR : cid.USD;
+  const drawerCash = cidType.reduce((acc, item) => item[1] + acc, 0);
 
   if (cashDue > drawerCash) {
     showStatus("Status: INSUFFICIENT_FUNDS");
   } else {
-    returnChange(cashDue);
+    returnChange(cashDue, cidType);
   }
 };
 
@@ -54,14 +86,14 @@ const showStatus = (status) => {
   changeDueEl.textContent = status;
 };
 
-const returnChange = (cashDue) => {
+const returnChange = (cashDue, cid) => {
   changeDue = [];
   let remainingDue = cashDue;
 
   cid.toReversed().forEach((item, index) => {
     const name = item[0];
     let cash = item[1];
-    const currency = currencyValues[name];
+    const currency = currencyValues[0][name];
 
     while (parseFloat(remainingDue.toFixed(2)) >= currency && cash > 0) {
       changeDue[index]
@@ -75,11 +107,11 @@ const returnChange = (cashDue) => {
   if (remainingDue >= 0.01) {
     showStatus("Status: INSUFFICIENT_FUNDS");
   } else {
-    withdrawFromDrawer();
+    withdrawFromDrawer(cid);
   }
 };
 
-const withdrawFromDrawer = () => {
+const withdrawFromDrawer = (cid) => {
   changeDue.forEach((changeItem) => {
     cid.forEach((cidItem, i) => {
       if (cidItem[0] === changeItem[0]) {
@@ -88,16 +120,23 @@ const withdrawFromDrawer = () => {
     });
   });
 
-  displayChange();
+  displayChange(cid);
 };
 
-const displayChange = () => {
+const displayChange = (cid) => {
   const drawerCash = cid.reduce((acc, item) => item[1] + acc, 0);
   changeDueEl.innerHTML = `Status: ${drawerCash > 0 ? "OPEN" : "CLOSED"}`;
 
   changeDue.forEach((item) => {
-    changeDueEl.innerHTML += `<br><span>${item[0]}: $${item[1]}`;
+    changeDueEl.innerHTML += `<br><span>${item[0]}: ${
+      isPKR ? `Rs. ${item[1]}` : `$${item[1]}`
+    }`;
   });
+};
+
+const reset = () => {
+  inputEl.value = "";
+  changeDueEl.innerHTML = "";
 };
 
 purchaseBtn.addEventListener("click", () => {
@@ -113,3 +152,10 @@ window.addEventListener("keydown", (e) => {
     purchaseBtn.click();
   }
 });
+
+currencyType.addEventListener("change", () => {
+  isPKR = false;
+  reset();
+});
+
+clearBtn.addEventListener("click", reset);
